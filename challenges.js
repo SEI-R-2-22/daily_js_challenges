77c7f9de41802f529d0cfc70ce43a0cf3a3d3286
vinhnghiemcr <vinhnghiemcr@gmail.com>
2022-03-01 15:36:52 -0600

    complete 7

--- challenges.js
+++ challenges.js
@@ -183,7 +183,13 @@ Examples:
 reverseUpcaseString("SEI Rocks!"); //=> "!SKCOR IES" 
 -----------------------------------------------------------------*/
 // Your solution for 07-reverseUpcaseString here:
-function reverseUpcaseString(string) {}
+function reverseUpcaseString(string) {
+  let result = ''
+  for(let i = string.length -1; i >= 0 ; i--){
+    result += string.charAt(i)
+  }
+  return result.toUpperCase()
+}
 /*-----------------------------------------------------------------
 Challenge: 08-removeEnds
 
