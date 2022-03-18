c34bfcc04d06f96c3da9c9176d84cf48a4f59da0
vinhnghiemcr <vinhnghiemcr@gmail.com>
2022-03-17 23:44:31 -0500

    Error on test 15

--- challenges.js
+++ challenges.js
@@ -399,7 +399,12 @@ mergeObjects({a: 1, b: 2, c: 3}, {d: 4});  //=> {a: 1, b: 2, c: 3, d: 4}
 mergeObjects({a: 1, b: 2, c: 3}, {d: 4}, {b: 22, d: 44});  //=> {a: 1, b: 22, c: 3, d: 44}
 -----------------------------------------------------------------*/
 // Your solution for 15-mergeObjects here:
-function mergeObjects(obj1, obj2) {}
+function mergeObjects(obj1, obj2) {
+  for (let i = 1; i < arguments.length; i++){
+    obj1 = {...obj1, ...arguments[i]}
+  }
+  return obj1
+}
 /*-----------------------------------------------------------------
 Challenge: 16-findHighestPriced
 
