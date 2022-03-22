7fb20dde9f4c5fbd79d1d19e2d7cc1b3ce1931de
Ethan Nitz <etnitz@gmail.com>
2022-03-22 12:32:45 -0500

    did some work

--- challenges.js
+++ challenges.js
@@ -179,7 +179,9 @@ Examples:
 reverseUpcaseString("SEI Rocks!"); //=> "!SKCOR IES" 
 -----------------------------------------------------------------*/
 // Your solution for 07-reverseUpcaseString here:
-function reverseUpcaseString(string) {}
+function reverseUpcaseString(string) {
+  return string.split('').reverse().join('').toUpperCase()
+}
 /*-----------------------------------------------------------------
 Challenge: 08-removeEnds
 
@@ -196,7 +198,13 @@ removeEnds('SEI Rocks!'); //=> "DI Rocks"
 removeEnds('a'); //=> "" (empty string)
 -----------------------------------------------------------------*/
 // Your solution for 08-removeEnds here:
-function removeEnds(string) {}
+function removeEnds(string) {
+  if (string.length < 3) {
+    return ''
+  } else {
+    return string.slice(1,-1)
+  }
+}
 /*-----------------------------------------------------------------
 Challenge: 09-charCount
 
@@ -215,7 +223,21 @@ charCount('hello') //=> { h: 1, e: 1, l: 2, o: 1 }
 charCount('Today is fantastic!') //=> { T: 1, o: 1, d: 1, a: 3, y: 1, ' ': 2, i: 2, s: 2, f: 1, n: 1, t: 2, c: 1, '!': 1 }
 -----------------------------------------------------------------*/
 // Your solution for 09-charCount here:
-function charCount(string) {}
+function charCount(string) {
+  let obj = {}
+  for (i = 0; i < string.length; i++) {
+    if (obj) {
+      if (obj.hasOwnProperty(string[i])) {
+        obj[string[i]] += 1
+      } else {
+        obj[string[i]] = 1
+      }
+    } else {
+      obj[string[i]] = 1
+    }
+  }
+  return obj
+}
 /*-----------------------------------------------------------------
 Challenge: 10-formatWithPadding
 
