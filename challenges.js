68f819328e5df7d0ba1a888811c9db0c434ea3fb
Daniel Kuntz <kuntzd99@gmail.com>
2022-04-08 16:36:32 -0500

    17 completed

--- challenges.js
+++ challenges.js
@@ -479,7 +479,13 @@ mapArray( ['rose', 'tulip', 'daisy'], function(f, i) {
 //=> ["1 - rose", "2 - tulip", "3 - daisy"]
 -----------------------------------------------------------------*/
 // Your solution for 17-mapArray here:
-function mapArray(arr, cb) {}
+function mapArray(arr, cb) {
+  const result = []
+  arr.forEach((element, index) => {
+    result.push(cb(element, index))
+  })
+  return result
+}
 /*-----------------------------------------------------------------
 Challenge: 18-reduceArray
 
