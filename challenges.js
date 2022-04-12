4464e5c045255ae736b20ca9a149a92ef61f9e2f
Jenna Leopold <jleopold424@gmail.com>
2022-04-12 09:17:24 -0400

    completed 20-isPrime

--- challenges.js
+++ challenges.js
@@ -570,7 +570,20 @@ isPrime(29) //=> true
 isPrime(200) //=> false
 -----------------------------------------------------------------*/
 // Your solution for 20-isPrime here:
-function isPrime(n) {}
+function isPrime(n) {
+  if (n < 2) {
+    return false
+  }
+  if (!Number.isInteger(n)) {
+    return false
+  }
+  for (let i = 2; i <= n / 2; i++) {
+    if (Number.isInteger(n / i)) {
+      return false
+    }
+  }
+  return true
+}
 /*-----------------------------------------------------------------
 Challenge: 21-primeFactors
 
