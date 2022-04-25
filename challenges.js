96278e08381351a25c3b104889a98df54ff423d7
Daniel Kuntz <kuntzd99@gmail.com>
2022-04-25 18:34:03 -0500

    #20 complete

--- challenges.js
+++ challenges.js
@@ -582,7 +582,23 @@ isPrime(29) //=> true
 isPrime(200) //=> false
 -----------------------------------------------------------------*/
 // Your solution for 20-isPrime here:
-function isPrime(n) {}
+function isPrime(n) {
+  if (n <= 1) {
+    return false
+  }
+  if (parseInt(n) !== n) {
+    return false
+  }
+  if (n === 2) {
+    return true
+  }
+  for (let i = 2; i < n; i++) {
+    if (n % i === 0) {
+      return false
+    }
+  }
+  return true
+}
 /*-----------------------------------------------------------------
 Challenge: 21-primeFactors
 
