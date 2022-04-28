3ccb94394e15a5483440bd6323aa94e7a10d14e3
Mark Harmon <neothej1sec@gmail.com>
2022-04-28 08:38:40 -0500

    map array complete and works with test

--- challenges.js
+++ challenges.js
@@ -447,7 +447,13 @@ mapArray( ['rose', 'tulip', 'daisy'], function(f, i) {
 //=> ["1 - rose", "2 - tulip", "3 - daisy"]
 -----------------------------------------------------------------*/
 // Your solution for 17-mapArray here:
-function mapArray(arr, cb) {}
+function mapArray(arr, cb) {
+  let res = []
+  for(let i = 0; i<arr.length; i++){
+    res.push(cb(arr[i], i))
+  }
+  return res
+}
 /*-----------------------------------------------------------------
 Challenge: 18-reduceArray
 
