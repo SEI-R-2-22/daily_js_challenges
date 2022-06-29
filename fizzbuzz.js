668a61b28646218c11470a9061ff23ebaa1f0552
Nick Lopez <nicklopez5454@yahoo.com>
2022-06-29 08:59:57 -0500

    finished pig latin problem

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,7 +1,3 @@
-// Square Every Digit
-
-const { sumNumbers } = require('./challenges')
-
 // Welcome. In this kata, you are asked to square every digit of a number and concatenate them.
 
 // For example, if we run 9119 through the function, 811181 will come out, because 92 is 81 and 12 is 1.
@@ -17,18 +13,49 @@ const { sumNumbers } = require('./challenges')
 //   return digit
 // }
 
-const squareDigits = (num) => {
-  let nStr = num
-    .toString()
-    .split('')
-    .map((x) => x * x)
-    .map((x) => x.toString())
-  let str = ''
-  for (let i = 0; i < nStr.length; i++) {
-    str += nStr[i]
-  }
+// const squareDigits = (num) => {
+//   let nStr = num
+//     .toString()
+//     .split('')
+//     .map((x) => x * x)
+//     .map((x) => x.toString())
+//   let str = ''
+//   for (let i = 0; i < nStr.length; i++) {
+//     str += nStr[i]
+//   }
 
-  return parseInt(str)
+//   return parseInt(str)
+// }
+
+// console.log(squareDigits(2224))
+
+// Move the first letter of each word to the end of it,
+// then add "ay" to the end of the word. Leave punctuation
+// marks untouched.
+
+const pigIt = (str) => {
+  let punc
+  if (str.includes('.')) {
+    punc = '.'
+  }
+  if (str.includes('?')) {
+    punc = '?'
+  }
+  if (str.includes('!')) {
+    punc = '!'
+  }
+  let newStr = str
+    .split(' ')
+    .filter((x) => x !== '.')
+    .filter((x) => x !== '!')
+    .filter((x) => x !== '?')
+    .map((x) => (x += x[0] + 'ay'))
+    .map((x) => x.substr(1))
+    .join(' ')
+  if (punc === '.') return newStr + ' .'
+  if (punc === '?') return newStr + ' ?'
+  if (punc === '!') return newStr + ' !'
+  return newStr
 }
 
-console.log(squareDigits(2224))
+console.log(pigIt('Pig latin is cool .'))
