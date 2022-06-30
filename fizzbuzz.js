7e99e067f7871f58faed87673f5f0d82d8baa409
Nick Lopez <nicklopez5454@yahoo.com>
2022-06-30 11:18:11 -0500

    finished Pangram problem

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -33,29 +33,99 @@
 // then add "ay" to the end of the word. Leave punctuation
 // marks untouched.
 
-const pigIt = (str) => {
-  let punc
-  if (str.includes('.')) {
-    punc = '.'
+// const pigIt = (str) => {
+//   let punc
+//   if (str.includes('.')) {
+//     punc = '.'
+//   }
+//   if (str.includes('?')) {
+//     punc = '?'
+//   }
+//   if (str.includes('!')) {
+//     punc = '!'
+//   }
+//   let newStr = str
+//     .split(' ')
+//     .filter((x) => x !== '.')
+//     .filter((x) => x !== '!')
+//     .filter((x) => x !== '?')
+//     .map((x) => (x += x[0] + 'ay'))
+//     .map((x) => x.substr(1))
+//     .join(' ')
+//   if (punc === '.') return newStr + ' .'
+//   if (punc === '?') return newStr + ' ?'
+//   if (punc === '!') return newStr + ' !'
+//   return newStr
+// }
+
+// console.log(pigIt('Pig latin is cool .'))
+
+// The marketing team is spending way too much time typing in hashtags.
+// Let's help them with our own Hashtag Generator!
+
+// Here's the deal:
+
+// It must start with a hashtag (#).
+// All words must have their first letter capitalized.
+// If the final result is longer than 140 chars it must return false.
+// If the input or the result is an empty string it must return false.
+
+// const generateHashtag = (str) => {
+//   if (str === '') return false
+//   let newStr = str.split(' ').map((x) => x.charAt(0).toUpperCase() + x.slice(1))
+//   newStr.unshift('#')
+//   let hashtag = newStr.join('')
+//   return hashtag
+// }
+
+// console.log(generateHashtag('Do We have A Hashtag'))
+
+// A pangram is a sentence that contains every single letter of the alphabet at least once.
+// For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram,
+// because it uses the letters A-Z at least once (case is irrelevant).
+// Given a string, detect whether or not it is a pangram. Return True if it is, False if not.
+// Ignore numbers and punctuation.
+
+const isPangram = (string) => {
+  const alphabet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z'
+  ]
+  let count = 0
+  for (let i = 0; i < alphabet.length; i++) {
+    if (
+      string.includes(alphabet[i]) ||
+      string.includes(alphabet[i].toUpperCase())
+    )
+      count++
+    console.log(count)
   }
-  if (str.includes('?')) {
-    punc = '?'
-  }
-  if (str.includes('!')) {
-    punc = '!'
-  }
-  let newStr = str
-    .split(' ')
-    .filter((x) => x !== '.')
-    .filter((x) => x !== '!')
-    .filter((x) => x !== '?')
-    .map((x) => (x += x[0] + 'ay'))
-    .map((x) => x.substr(1))
-    .join(' ')
-  if (punc === '.') return newStr + ' .'
-  if (punc === '?') return newStr + ' ?'
-  if (punc === '!') return newStr + ' !'
-  return newStr
+  if (count === 26) return true
+  return false
 }
 
-console.log(pigIt('Pig latin is cool .'))
+console.log(isPangram('The quick brown fox jumps over the lazy dog'))
